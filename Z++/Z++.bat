51b0de3a920b13987f7510c1115f85294f2f7eb9
Bray <116981625+MODZERS@users.noreply.github.com>
2023-05-13 09:36:55 -0400

    Add files via upload

---
+++ Z++/Z++.bat
@@ -0,0 +1,71 @@
+@echo off
+cls
+:: Paste extention links here
+:: Also Paste Api links here to
+:: Include them into Z++
+:: Make sure it is a reletive path
+:: Ex: -- call Extentions\test.bat
+[
+
+
+
+]
+
+cls
+
+
+
+:: Basic Api Scripts
+
+set print=echo
+set Print=echo
+
+set col=color
+set Col=color
+
+set Window.color=color
+set window.Color=color
+set Window.Color=color
+set window.color=color
+
+set end=exit
+set End=exit
+set wait=timeout
+set Wait=timeout
+set time=timeout
+set Time=timeout
+
+set include=call
+set Include=call
+
+set Export.c=copy
+set export.c=copy
+
+set export=move
+set Export=move
+
+set user=%username%
+set User=%username%
+
+set stop=pause>null
+set Stop=pause>null
+
+set class=set
+set Class=set
+
+set int=set
+set Int=set
+
+set function=set
+set Function=set
+set func=set
+set Func=set
+
+:: Exploits
+
+set target=call
+set Target=call
+
+set attach=call Exploits\Attach.bat
+set Attach=call Exploits\Target.bat
+
